329d4d07a2e1fe1ef26010840b0a285ededd0a43
BosimovOxunjon <bosimovoxunjon@gmail.com>
2022-12-28 17:57:16 +0500

    some bugs have been fiexed

---
+++ src/configs/configs.js
@@ -0,0 +1,5 @@
+const keys = {
+  BACKEND_API: "https://shopzone.onrender.com",
+};
+
+export default keys;
--- src/containers/SignUp/SignUp.jsx
+++ src/containers/SignUp/SignUp.jsx
@@ -1,36 +1,121 @@
+import axios from "axios";
 import React, { useState } from "react";
 import {
+  createTheme,
+  ThemeProvider,
   Container,
   Box,
   Grid,
   TextField,
-  Text,
   Button,
   Link,
+  ImageList,
+  ImageListItem,
 } from "@mui/material";
+import { orange } from "@mui/material/colors";
 import { MuiTelInput, matchIsValidTel } from "mui-tel-input";
+import BackgroundImg from "../../assets/img/SignUp/background.png";
+import MainImg from "../../assets/img/SignUp/signup.png";
+import keys from "../../configs/configs";
 
 const SignUp = () => {
-  const handleSubmit = async (e) => {
-    console.log(e);
-  };
   const [phone, setPhone] = useState("");
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+  const handleSubmit = async (value) => {
+    value.preventDefault();
+    const { data } = await axios.post(`${keys.BACKEND_API}/auth/sign-up`, {
+      ...value,
+    });
+    // data =;
+    setUser(data);
+    return data;
+  };
 
   const handleChangePhone = (newPhone) => {
     setPhone(newPhone);
     matchIsValidTel(newPhone);
   };
+
+  const theme = createTheme({
+    status: {
+      danger: orange[500],
+    },
+  });
+
   return (
     <>
       <Container
-        component="main"
-        sx={{ display: "flex", flexWrap: { xs: "wrap", md: "nowrap" } }}
+        component="section"
+        sx={{
+          display: "flex",
+          p: "0!important",
+          flexWrap: { xs: "wrap", md: "nowrap" },
+        }}
       >
-        <Grid container columnSpacing={3} sx={{ height: "100vh" }}>
-          <Box component="div" xs={12} sm={6}></Box>
+        <Grid
+          container
+          columnSpacing={{ md: 6, xs: 12 }}
+          sx={{
+            display: "flex",
+            height: "100vh",
+            ml: "0!important",
+            overflow: "hidden",
+          }}
+        >
+          <Box component="div" className="" xs={12} sm={6}>
+            <ImageList
+              sx={{
+                justifyItems: "center",
+                mt: 0,
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              {/* <ImageListItem cols={10}> */}
+              <Box
+                component="img"
+                src={BackgroundImg}
+                sx={{
+                  width: { xs: "100vw", md: "50vw" },
+                }}
+              />
+              {/* </ImageListItem> */}
+              <Box
+                component="img"
+                src={MainImg}
+                sx={{
+                  mt: 3,
+                  position: "absolute",
+                }}
+              />
+            </ImageList>
+          </Box>
         </Grid>
-        <Grid container columnSpacing={9} sx={{ m: "auto", width: "inherit" }}>
-          <Box sx={{ fontSize: "h1.fontSize" }}>Sign Up</Box>
+        <Grid
+          container
+          columnSpacing={{ md: 6, xs: 12 }}
+          m="auto"
+          sx={{
+            m: "auto!important",
+            width: "inherit",
+            pt: { xs: 5, md: 0 },
+            pl: 3,
+          }}
+        >
+          <Box
+            sx={{
+              fontSize: { lg: "h1.fontSize", xs: "h3.fontSize" },
+              fontWeight: 600,
+            }}
+          >
+            Sign Up
+          </Box>
           <Box
             component="form"
             xs={12}
